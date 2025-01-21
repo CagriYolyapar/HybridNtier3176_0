f8e1804b93a51639b06bed2b2304d0c2aa03c335
cagri.yolyapar <cagri.yolyapar@gmail.com>
2025-01-21 10:26:11 +0300

    Sepet sayfası icin RenderSection acılarak AddToCart metodu entegre edildi

--- Project.MvcUI/Controllers/ShoppingController.cs
+++ Project.MvcUI/Controllers/ShoppingController.cs
@@ -8,6 +8,8 @@ using Project.Entities.Models;
 using Project.MvcUI.Models.PageVms;
 using X.PagedList;
 using X.PagedList.Extensions;
+using Project.MvcUI.Models.SessionService;
+using Project.MvcUI.Models.ShoppingTools;
 
 namespace Project.MvcUI.Controllers
 {
@@ -63,6 +65,43 @@ namespace Project.MvcUI.Controllers
 
 
             return View(spVm);
+        }
+
+        //Dikkat edin bu metotlar private'tir...Yani hicbir zaman bir Request'e cevap vermeyeceklerdir...Onlar sadece ve sadece Controller'in icerisinden ulasılabilen Controller'a özel korunmus yapılardır...
+
+        #region ControllerPrivateMetotlari
+        void SetCartForSession(Cart c)
+        {
+            HttpContext.Session.SetObject("scart", c);
+        }
+
+        Cart GetCartFromSession(string key)
+        {
+            return HttpContext.Session.GetObject<Cart>(key);
+        }
+
+        #endregion
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            Cart c = GetCartFromSession("scart") == null ? new Cart() : GetCartFromSession("scart");
+
+            Product productToBeAdded = _mapper.Map<Product>(await _productManager.GetByIdAsync(id));
+
+            CartItem ci = new()
+            {
+                Id = productToBeAdded.Id,
+                ProductName = productToBeAdded.ProductName,
+                UnitPrice = productToBeAdded.UnitPrice,
+                ImagePath = productToBeAdded.ImagePath,
+                CategoryId = productToBeAdded.CategoryId,
+                CategoryName = productToBeAdded.Category.CategoryName
+            };
+
+            c.AddToCart(ci);
+
+            SetCartForSession(c);
+            TempData["Message"] = $"{ci.ProductName} isimli ürün sepete eklenmiştir";
+            return RedirectToAction("Index");
         }
     }
 }
